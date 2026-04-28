486aaa5395df9d04018926c675fab63eb8ca3e83
jgjinko <johangjinko2007@gmail.com>
2026-04-28 18:22:10 +0200

    bug_fixes

--- smart_bug_bounty/bug_fixes/Bug3Check.java
+++ smart_bug_bounty/bug_fixes/Bug3Check.java
@@ -1,1 +1,11 @@
-public class Bug3Check { public static void main(String[] args) { int passed = 0; if (bug3_fixed.firstUniqueChar("swiss") == "w".charAt(0)) passed++; if (bug3_fixed.firstUniqueChar("aabbcc") == "".charAt(0)) passed++; if (bug3_fixed.firstUniqueChar("") == "".charAt(0)) passed++; System.out.println("bug3_fixed.java: " + passed + "/3 passed"); }}
+public class Bug3Check { 
+    public static void main(String[] args) { 
+        int passed = 0; 
+        
+        if (bug3.firstUniqueChar("swiss") == "w".charAt(0)) passed++; 
+        if (bug3.firstUniqueChar("aabbcc") == '\0') passed++; 
+        if (bug3.firstUniqueChar("") == '\0') passed++; 
+        
+        System.out.println("bug3.java: " + passed + "/3 passed"); 
+    }
+}
